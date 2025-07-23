de1e0e1a162f4b4226635df1692c3011b77a1ca6
swafwana <swafwanasheri17@gmail.com>
2025-07-23 11:17:51 +0530

    Add files via upload

--- prgrm15.py
+++ prgrm15.py
@@ -5,4 +5,22 @@ def rotationMatrixx(degree):
     c,s=numpy.cos(theta),numpy.sin(theta)
     return numpy.matrix([[1,0,0,0],[0,c,-s,0],[0,s,c,0],[0,0,0,1]])
 matrix1=rotationMatrixx(30)
+print("Rotation about x axis")
 print(matrix1)
+# Defines 3d rotation matrix for rotating about y axis
+
+def rotationMatrixy(degree):
+    theta = numpy.radians(degree)
+    c,s=numpy.cos(theta),numpy.sin(theta)
+    return numpy.matrix([[c,0,s,0],[0,1,0,0],[-s,0,c,0],[0,0,0,1]])
+matrix2=rotationMatrixy(30)
+print("Rotation about y axis")
+print(matrix2)
+# Defines 3d rotation matrix for rotating about z axis
+def rotationMatrixz(degree):
+    theta=numpy.radians(degree)
+    c,s=numpy.cos(theta),numpy.sin(theta)
+    return numpy.matrix([[c,-s,0,0],[s,c,0,0],[0,0,1,0],[0,0,0,1]])
+matrix3=rotationMatrixz(30)
+print("Rotation about z axis")
+print(matrix3)
