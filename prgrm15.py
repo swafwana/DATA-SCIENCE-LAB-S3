c4936e8518cefd07cfd7710efefe38ba26278732
swafwana <swafwanasheri17@gmail.com>
2025-08-04 11:59:10 +0530

    Update prgrm15.py

--- prgrm15.py
+++ prgrm15.py
@@ -1,9 +1,6 @@
 import numpy
 def translationMatrix(tx=0, ty=0, tz=0):
-return numpy.matrix([[1,0,0,tx],
-[0,1,0,ty],
-[0,0,1,tz],
-[0,0,0,1 ]])
+    return numpy.matrix([[1,0,0,tx],[0,1,0,ty],[0,0,1,tz],[0,0,0,1 ]])
 matrix=translationMatrix(1,1,1)
 print(matrix)
 
@@ -34,4 +31,3 @@ def rotationMatrixz(degree):
 matrix3=rotationMatrixz(30)
 print("Rotation about z axis")
 print(matrix3)
-
