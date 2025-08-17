90e504f4830ab17bf92375571922bd69f9a3d371
swafwana <swafwanasheri17@gmail.com>
2025-08-17 08:40:10 +0530

    Update prgrm14.py

--- prgrm14.py
+++ prgrm14.py
@@ -1,21 +1,19 @@
-#TRANSLATION
 import numpy
-def translationMatrix(tx=0, ty=0, tz=0):
-    return numpy.matrix([[1,0,0,tx],[0,1,0,ty],[0,0,1,tz],[0,0,0,1 ]])
-matrix=translationMatrix(1,1,1)
+def translationMatrix(tx=0, ty=0):
+    return numpy.matrix([[1,0,tx],[0,1,ty],[0,0,1]])
+matrix=translationMatrix(1,1)
 print(matrix)
-#ROTATION
 def rotationMatrix(degree):
     theta = numpy.radians(degree)
     c,s=numpy.cos(theta),numpy.sin(theta)
     return numpy.matrix([[c,-s,0],[s, c,0],[0,0,1]])
-matrix2=rotationMatrix(30)
-print(matrix2)
-#SCALING MATRIX
+matrix=rotationMatrix(30)
+print(matrix)
 def scalingMatrix(sx=0, sy=0):
     return numpy.matrix([[sx,0,0],[0,sy,0],[0,0,1]])
-matrix3=scalingMatrix(2,2)
-print(matrix3)
+matrix=scalingMatrix(2,2)
+print(matrix)
+
 
 
 
